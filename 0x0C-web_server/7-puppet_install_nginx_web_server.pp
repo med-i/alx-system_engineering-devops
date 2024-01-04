7862dbcdad190df25df437dd2696b5a7a55354bc
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-04 03:45:26 +0100

    Add advanced task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# install Nginx web server using Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    server_name _;
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://github.com/med-i;
+    }
+}
+",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}

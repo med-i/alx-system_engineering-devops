84c14933b8f27b04f9fda9b0ab876fdf1443f402
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-08 23:50:23 +0100

    Fix task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,39 +5,35 @@ package { 'nginx':
 }
 
 file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+  content => 'Hello, World!',
 }
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
+  ensure  => present,
+  content => "
 server {
-  add_header X-Served-By $hostname;
-  listen 80 default_server;
-  listen [::]:80 default_server;
+    add_header X-Served-By \$hostname;
+    listen 80 default_server;
+    server_name _;
 
-  root /var/www/html;
+    location /redirect_me {
+      return 301 https://github.com/med-i;
+    }
 
-  server_name _;
-  error_page 404 /404.html;
-  location = /404.html {
-    root /var/www/html; internal;
-  }
-  location /redirect_me {
-    return 301 https://github.com/med-i;
-  }
-  location / {
-    try_files $uri $uri/ =404;
-  }
-}',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+    error_page 404 /404.html;
+    location = /404.html {
+      root /var/www/html;
+      internal;
+    }
+
+    location / {
+      root /var/www/html;
+      index index.html;
+    }
+  }",
 }
 
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+  ensure    => running,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }

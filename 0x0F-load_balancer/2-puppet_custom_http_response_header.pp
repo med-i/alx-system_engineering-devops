f713b05d37c1bebecc5cf1e3f655ef4e01d05caf
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-09 15:35:44 +0100

    Fix task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,23 @@
 # install Nginx web server using Puppet
 
-package { 'nginx':
-  ensure => installed,
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
 }
-
+->
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
 file_line { 'add HTTP header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;'
 }
-
+->
 service { 'nginx':
   ensure  => 'running',
   enable  => true,

7e7957c8bf4a6011c78a3717e82814e80df20789
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-30 02:26:36 +0100

    Add task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""
+This module exports to JSON
+"""
+
+import json
+import requests
+from sys import argv
+
+
+def export_todos_to_json(id):
+    """Exports data in the JSON format"""
+    api_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{api_url}/users/{id}"
+    todos_url = f"{user_url}/todos"
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+
+    username = user["username"]
+    tasks = [
+        {
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username,
+        }
+        for task in todos
+    ]
+
+    tasks_json = {id: tasks}
+    filename = f"{id}.json"
+
+    with open(filename, "w") as file:
+        json.dump(tasks_json, file)
+
+
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        export_todos_to_json(argv[1])

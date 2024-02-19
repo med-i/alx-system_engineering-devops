e4b5b8ede9e397074e2965f66894266bf986a2b6
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-19 22:09:12 +0100

    Add task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers of a subreddit."""
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
     headers = {"User-Agent": "linux:alx:v1.0.0 (by /u/I-Med)"}
 
     response = requests.get(api_url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
     return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines top_ten method.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the 10 first titles of hot posts listed for a given subreddit."""
+    api_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "linux:alx:v1.0.0 (by /u/I-Med)"}
+
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+    else:
+        data = response.json().get("data").get("children")
+        for post in data[:10]:
+            print(post.get("data").get("title"))

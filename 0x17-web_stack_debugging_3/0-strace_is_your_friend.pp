ece757c7b9262cf94016ba3cc64e02623d4b2355
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-02-20 22:48:27 +0100

    Add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# rename class-wp-locale.phpp to class-wp-locale.php
 exec { 'rename_class-wp-locale':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/bin'],
